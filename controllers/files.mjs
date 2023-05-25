79c3dbfda731139f9c6990666875e74020697c6f
stanley nduaguibe <chido.nduaguibe@gmail.com>
2023-05-25 15:36:45 +0100

    added azure file upload

--- controllers/files.mjs
+++ controllers/files.mjs
@@ -2,35 +2,50 @@ import express from "express";
 import fileUpload from "express-fileupload"
 import cloudinary from "cloudinary"
 import streamifier from "streamifier"
+import AzureStorageBlob from "@azure/storage-blob"
 import utils from "../utils/index.mjs";
+
 const Router = express.Router();
 
-
+let azure;
 
 
 const cloudUpload = function (file) {
-    try {
-        let resource_type = 'raw'
-        if (file.mimetype.includes("image") || file.mimetype.includes("video")) {
-            resource_type = 'auto';
-        }
-        return new Promise((resolve, reject) => {
-            const stream = cloudinary.v2.uploader.upload_stream({ folder: "learning", resource_type },
-                function (error, result) {
-                    if (error) {
-                        reject(error);
-                        return;
-                    }
-                    resolve({ ...result })
-                })
-            streamifier.createReadStream(file.data).pipe(stream);
-        })
-    } catch (error) {
-        console.log(error)
+    let resource_type = 'raw'
+    if (file.mimetype.includes("image") || file.mimetype.includes("video")) {
+        resource_type = 'auto';
     }
+    return new Promise((resolve, reject) => {
+        const stream = cloudinary.v2.uploader.upload_stream({ folder: UploadFile.folder, resource_type },
+            function (error, result) {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                resolve({ ...result })
+            })
+        streamifier.createReadStream(file.data).pipe(stream);
+    })
 }
 
-const uploadWithCloudinary = utils.wrapper(async (req, res) => {
+const azureUpload = async function (file) {
+    const uploadPath = `${UploadFile.folder}/${file.name}`;
+    // create blobClient for container
+    const blobClient = azure.getBlockBlobClient(uploadPath);
+
+    // set mimetype as determined from browser with file upload control
+    const options = { blobHTTPHeaders: { blobContentType: file.type } };
+    // upload file
+    await blobClient.uploadData(file.data, options);
+    return {
+        url: blobClient.url,
+        secure_url: blobClient.url,
+        filePath: uploadPath
+    }
+
+}
+
+const uploadFile = utils.wrapper(async (req, res) => {
     try {
         const files = req.files;
         if (!files) {
@@ -46,19 +61,17 @@ const uploadWithCloudinary = utils.wrapper(async (req, res) => {
             let fileObj = files[file];
             if (Array.isArray(fileObj)) {
                 fileObj = fileObj.map(async (file) => {
-                    return await cloudUpload(file)
+                    return await UploadFile.selectedService(file)
                 })
-
             } else {
-                fileObj = cloudUpload(fileObj);
+                fileObj = UploadFile.selectedService(fileObj);
             }
-            console.log({ fileObj })
             uploaded[file] = await (Array.isArray(fileObj) ? Promise.all(fileObj) : fileObj)
         }
         return {
             code: 201,
             data: {
-                ...uploaded
+                data: uploaded
             }
         }
     } catch (error) {
@@ -73,19 +86,22 @@ const uploadWithCloudinary = utils.wrapper(async (req, res) => {
 })
 const UploadFile = {
     selectedService: null,
-    setup: function ({ type = utils.FILE_TYPES.AZURE, options }) {
+    folder: null,
+    setup: function ({ type = utils.FILE_TYPES.AZURE, options, folder = 'my_folder' }) {
+        this.folder = folder
         if (type === utils.FILE_TYPES.CLOUDINARY) {
             cloudinary.config(options);
-            this.selectedService = uploadWithCloudinary
+            this.selectedService = cloudUpload
         } else {
-
+            azure = new AzureStorageBlob.BlobServiceClient(options.url).getContainerClient(options.container);
+            this.selectedService = azureUpload
         }
 
         return this;
     },
     addUploadFileRoute() {
         Router.post("/files", [fileUpload({
-        })], this.selectedService)
+        })], uploadFile)
         return Router;
     }
 }
